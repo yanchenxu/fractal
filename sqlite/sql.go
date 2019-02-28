0ce8a328e86713b6c2db9b098464251641e64cb6
Apple <apple@AppledeMacBook-Pro-2.local>
2019-02-28 19:54:23 +0800

    add sql

--- sqlite/sql.go
+++ sqlite/sql.go
@@ -1,1 +1,11 @@
 package sqlite
+
+var (
+	CREATE_SQL = `
+	CREATE TABLE IF NOT EXISTS userinfo(
+	uid INTEGER PRIMARY KEY AUTOINCREMENT,
+	username VARCHAR(64) NULL,
+	departname VARCHAR(64) NULL,
+	created DATE NULL);
+`
+)
